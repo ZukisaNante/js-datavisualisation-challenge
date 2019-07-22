23c6ec0d33b964dfa0c028b3a4902a26ed8f7b36
ZukisaNante <zukisanante@gmail.com>
2019-07-22 22:13:41 +0200

    html to json tempted

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 //create variables
-let year = [2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012];
-const Belgique = ['1012,8', '1007,8', '1013,7', '2999,4', '1022,8', '1034,4', '1043,6', '1067,3', '1072,0', '1111,0', '1073,8'];
-const Bulgarie = ['146,9', '143,9', '142,1', '137,8', '136,4', '134,7', '126,7', '138,1', '147,0', '128,6', '1073,8'];
+//converingtable to json
+function tableToJson(table) { 
+    var data = []; // first row needs to be headers var headers = []; 
+    for (var i=0; i<table.rows[0].cells.length; i++) {
+     headers[i] = table.rows[0].cells[i].innerHTML.toLowerCase().replace(/ /gi,''); 
+    } 
+    // go through cells 
+    for (var i=1; i<table.rows.length; i++) { 
+    var tableRow = table.rows[i]; var rowData = {}; 
+    for (var j=0; j<tableRow.cells.length; j++) { 
+    rowData[ headers[j] ] = tableRow.cells[j].innerHTML; 
+    } data.push(rowData); 
+    } 
+    return data; 
+    }
 //draw chart
 function drawChart() {
     var newChart = document.getElementById('chartInfo');
