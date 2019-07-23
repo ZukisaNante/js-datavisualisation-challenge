3c97321796e73ce2c95fcf32ba4af5505ebf692c
junior <zukisanante@gmail.com>
2019-07-23 15:58:53 +0200

    canvas retrieves data from the table but with a string

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 //create variables
-let headers = document.getElementsByTagName('h3'); //calling the header
-var tableInfo = document.getElementById('table1'); //calling the table
+//let headers = document.getElementsByTagName('h3'); //calling the header
+//var tableInfo = document.getElementById('table1'); //calling the table
 
 //converingtable HTML to an array
 
-var data = []; // first row needs to be headers var headers = []; 
+/* var data = []; // first row needs to be headers var headers = []; 
 for (var i = 0; i < tableInfo.rows[0].cells.length; i++) {
     headers[i] = tableInfo.rows[0].cells[i].innerHTML.toLowerCase().replace(/ /gi, ''); //gi ? google
 }
@@ -13,16 +13,69 @@ for (var i = 1; i < tableInfo.rows.length; i++) {
     var tableRow = tableInfo.rows[i];
     var rowData = {};
     for (var j = 0; j < tableRow.cells.length; j++) {
+        [headers[j]][headers[j]]
         rowData[headers[j]] = tableRow.cells[j].innerHTML;
     }
     data.push(rowData);
 }
+console.log(data); */
+
+var table = document.getElementById("table1");
+var tableLen = table.rows.length;
+var data = { labels: [], country: [], area: [] };
+parseInt(data);
 console.log(data);
 
+for (var i = 1; i < tableLen; i++) {
+    data.labels.push(table.rows[i].cells[0].innerText)
+    data.country.push(table.rows[i].cells[1].innerText.replace(',', ''))
+    data.area.push(table.rows[i].cells[2].innerText)
+    let changePass = parseInt(data);
+    console.log(changePass);
+}
+
+var canvasP = document.getElementById("chartInfo")
+var ctxP = canvasP.getContext('2d')
+var myPieChart = new Chart(ctxP, {
+    type: 'line',
+    data: {
+        labels: data.labels,
+        datasets: [{
+            data: data.country,
+            backgroundColor: ["#64B5F6", "#FFD54F", "#2196F3", "#FFC107", "#1976D2", "#FFA000", "#0D47A1"],
+            hoverBackgroundColor: ["#B2EBF2", "#FFCCBC", "#4DD0E1", "#FF8A65", "#00BCD4", "#FF5722", "#0097A7"]
+        }]
+    },
+    options: {
+        legend: {
+            display: true,
+            position: "right"
+        }
+    }
+})
+
+
 //draw chart
-function drawChart() {
-    var newChart = document.getElementById('chartInfo');
-    console.log(newChart);
+/* var years = [];
+let noYears = years.push(tableRow.rows[0]);
+console.log(noYears);
+var labels = [];
+const datasets = [];
+console.log(years);
+var ctx = document.getElementById('chartInfo');
+var chartInfo = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: Pays,
+        datasets: [{
+            data: Belgique,
+        }]
+    }
+
+}); */
+/* function drawChart() {
+    var canvas = document.getElementById('chartInfo');//link to html
+    console.log(canvas);
     if (canvas.getContext) {
         var ctx = canvas.getContext('2d');
         var lineChart = new Chart(ctx, {
@@ -34,4 +87,4 @@ function drawChart() {
         });
 
     }
-}
+} */
